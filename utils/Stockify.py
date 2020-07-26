1b9ce4c6de09422d1aa3d9073c7252632132fb83
Dumrich <abhinavchavali12@gmail.com>
2020-07-26 09:52:07 -0400

    added EPS and other data functionality

--- utils/Stockify.py
+++ utils/Stockify.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import re
 
 
 class Stockify:
@@ -13,16 +13,34 @@ class Stockify:
         self.url_keys = ['year 1','year 2','year 3','year 4','year 5','year 6','year 7','year 8',\
                    'year 9','year 10',]
         self.url_dict = self.get_10K_links()
-
+        self.EPSList = self.get_EPS()
+        self.OperationStatement = self.get_operation_statement_numbers()
+        self.NetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
+        self.CostOfSalesID = 'defref_us-gaap_CostOfGoodsAndServicesSold'
+        self.NetMarginID = 'defref_us-gaap_GrossProfit'
+        self.NetIncomeID = 'defref_us-gaap_NetIncomeLoss'
+        self.ResearchDevelopmentID = 'defref_us-gaap_ResearchAndDevelopmentExpense'
+        self.SGAID = 'defref_us-gaap_SellingGeneralAndAdministrativeExpense'
+        self.OperatingExpensesID = 'defref_us-gaap_OperatingExpenses'
+        self.OperatingIncome = 'defref_us-gaap_OperatingIncomeLoss'
+        self.OtherIncome = 'defref_us-gaap_NonoperatingIncomeExpense'
+        self.IncomeBeforeProvisionIncomeTaxesID = 'defref_us-gaap_IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest'
+        self.ProvisionFOrIncomeTaxesID = 'defref_us-gaap_IncomeTaxExpenseBenefit'
+        self.NetIncomeId = 'defref_us-gaap_NetIncomeLoss'
+        self.DilutedEPSID = 'defref_us-gaap_EarningsPerShareDiluted'
+        self.TotalNetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
+        
     def get_CIK(self):
+        """Get the SEC CIK"""
         url = f"https://sec.report/Ticker/{self.ticker}"
         request = {'url': url, }
-        soup = BeautifulSoup(reqests.get(request['url'], \
+        soup = BeautifulSoup(requests.get(request['url'], \
                                           headers={'User-Agent':self.headers}).content, 'lxml')
 
         return(soup.h2.text.split()[-1])
 
     def get_10K_links(self):
+        """Get all 10K links"""
         request = {'url': self.base_url, 'User-Agent': self.headers}
         soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')
 
@@ -30,18 +48,69 @@ class Stockify:
         return {self.url_keys[i]: 'https://www.sec.gov'+url_list[i] for i in range(len(self.url_keys))}
 
     def get_EPS(self):
-        for url in self.url_dict.values():
-            accession_number = url.split('&')[2][17:].replace('-', '')
-            BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK()[4:]}/{accession_number}/R2.htm"
-            request = {'url': BASE_URL, 'User-Agent': self.headers}
-            soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')
+        """Get the EPS"""
+        EPSList = []
 
-            return 'EarningsPerShareBasic' in soup.prettify()
+        for url in list(self.url_dict.values())[:9]:
+            accession_number = url.split('&')[2][17:].replace('-', '')
+            try:
+                BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R2.htm"
+                request = {'url': BASE_URL, 'User-Agent': self.headers}
+                soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
+                        .prettify().split('defref_us-gaap_EarningsPerShareBasic')[1].split("\n")[5].strip()
+                EPSList.append(soup.strip(' '))
+            except:
+                BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R3.htm"
+                request = {'url': BASE_URL, 'User-Agent': self.headers}
+                soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
+                       .prettify().split('defref_us-gaap_EarningsPerShareBasic')[1].split("\n")[5].strip()
+
+                EPSList.append(soup.strip(' '))
+
+
+        return [item for item in EPSList if item[0] == '$']
+
+    def get_operation_statement_numbers(self):
+        NetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
+        CostOfSalesID = 'defref_us-gaap_CostOfGoodsAndServicesSold'
+        NetMarginID = 'defref_us-gaap_GrossProfit'
+        NetIncomeID = 'defref_us-gaap_NetIncomeLoss'
+        ResearchDevelopmentID = 'defref_us-gaap_ResearchAndDevelopmentExpense'
+        SGAID = 'defref_us-gaap_SellingGeneralAndAdministrativeExpense'
+        OperatingExpensesID = 'defref_us-gaap_OperatingExpenses'
+        OperatingIncome = 'defref_us-gaap_OperatingIncomeLoss'
+        OtherIncome = 'defref_us-gaap_NonoperatingIncomeExpense'
+        IncomeBeforeProvisionIncomeTaxesID = 'defref_us-gaap_IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest'
+        ProvisionFOrIncomeTaxesID = 'defref_us-gaap_IncomeTaxExpenseBenefit'
+        NetIncomeId = 'defref_us-gaap_NetIncomeLoss'
+        DilutedEPSID = 'defref_us-gaap_EarningsPerShareDiluted'
+        TotalNetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
+        
+
+        for url in list(self.url_dict.values())[:9]:
+            accession_number = url.split('&')[2][17:].replace('-', '')
+            try:
+                BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R2.htm"
+                request = {'url': BASE_URL, 'User-Agent': self.headers}
+                soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
+                        .prettify().split()[1].split("\n")[5].strip()
+                EPSList.append(soup.strip(' '))
+            except:
+                BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R3.htm"
+                request = {'url': BASE_URL, 'User-Agent': self.headers}
+                soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
+                        .prettify().split((NetSalesID, CostOfSalesID, NetMarginID, NetIncomeId, ResearchDevelopmentID, SGAID, OperatingExpensesID, OperatingIncome, OtherIncome, IncomeBeforeProvisionIncomeTaxesID, ProvisionFOrIncomeTaxesID, NetIncomeId, DilutedEPSID, TotalNetSalesID))[1].split("\n")[5].strip()
+
+                EPSList.append(soup.strip(' '))
+
+        return [item for item in EPSList if item[0] == '$']
+9
+
+
 
     def get_historical_stock_prices(self):
-
         pass
 
-print(Stockify('aapl').get_EPS())
 
+print(Stockify('amzn').get_EPS())
 
