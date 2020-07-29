233190bd7c8a687b282c786bb6e217fa2c01a4b6
Dumrich <abhinavchavali12@gmail.com>
2020-07-29 11:24:30 -0400

    added get operation statement functionality

--- utils/Stockify.py
+++ utils/Stockify.py
@@ -14,21 +14,7 @@ class Stockify:
                    'year 9','year 10',]
         self.url_dict = self.get_10K_links()
         self.EPSList = self.get_EPS()
-        self.OperationStatement = self.get_operation_statement_numbers()
-        self.NetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
-        self.CostOfSalesID = 'defref_us-gaap_CostOfGoodsAndServicesSold'
-        self.NetMarginID = 'defref_us-gaap_GrossProfit'
-        self.NetIncomeID = 'defref_us-gaap_NetIncomeLoss'
-        self.ResearchDevelopmentID = 'defref_us-gaap_ResearchAndDevelopmentExpense'
-        self.SGAID = 'defref_us-gaap_SellingGeneralAndAdministrativeExpense'
-        self.OperatingExpensesID = 'defref_us-gaap_OperatingExpenses'
-        self.OperatingIncome = 'defref_us-gaap_OperatingIncomeLoss'
-        self.OtherIncome = 'defref_us-gaap_NonoperatingIncomeExpense'
-        self.IncomeBeforeProvisionIncomeTaxesID = 'defref_us-gaap_IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest'
-        self.ProvisionFOrIncomeTaxesID = 'defref_us-gaap_IncomeTaxExpenseBenefit'
-        self.NetIncomeId = 'defref_us-gaap_NetIncomeLoss'
-        self.DilutedEPSID = 'defref_us-gaap_EarningsPerShareDiluted'
-        self.TotalNetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
+       # self.OperationStatement = self.get_operation_statement_numbers()
         
     def get_CIK(self):
         """Get the SEC CIK"""
@@ -71,40 +57,54 @@ class Stockify:
         return [item for item in EPSList if item[0] == '$']
 
     def get_operation_statement_numbers(self):
-        NetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
-        CostOfSalesID = 'defref_us-gaap_CostOfGoodsAndServicesSold'
-        NetMarginID = 'defref_us-gaap_GrossProfit'
-        NetIncomeID = 'defref_us-gaap_NetIncomeLoss'
-        ResearchDevelopmentID = 'defref_us-gaap_ResearchAndDevelopmentExpense'
-        SGAID = 'defref_us-gaap_SellingGeneralAndAdministrativeExpense'
-        OperatingExpensesID = 'defref_us-gaap_OperatingExpenses'
-        OperatingIncome = 'defref_us-gaap_OperatingIncomeLoss'
-        OtherIncome = 'defref_us-gaap_NonoperatingIncomeExpense'
-        IncomeBeforeProvisionIncomeTaxesID = 'defref_us-gaap_IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest'
-        ProvisionFOrIncomeTaxesID = 'defref_us-gaap_IncomeTaxExpenseBenefit'
-        NetIncomeId = 'defref_us-gaap_NetIncomeLoss'
-        DilutedEPSID = 'defref_us-gaap_EarningsPerShareDiluted'
-        TotalNetSalesID = 'defref_us-gaap_RevenueFromContractWithCustomerExcludingAssessedTax'
-        
+        NetSalesID = 'Revenue'
+        CostOfSalesID = 'us-gaap_CostOfGoodsAndServicesSold'
+        NetMarginID = 'us-gaap_GrossProfit'
+        NetIncomeID = 'us-gaap_NetIncomeLoss'
+        ResearchDevelopmentID = 'us-gaap_ResearchAndDevelopmentExpense'
+        SGAID = 'us-gaap_SellingGeneralAndAdministrativeExpense'
+        OperatingExpensesID = "us-gaap_OperatingExpenses'"
+        OperatingIncomeID = 'us-gaap_OperatingIncomeLoss'
+        OtherIncomeID = 'us-gaap_NonoperatingIncomeExpense'
+        IncomeBeforeProvisionIncomeTaxesID = 'us-gaap_IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest'
+        ProvisionForIncomeTaxesID = 'us-gaap_IncomeTaxExpenseBenefit'
+        NetIncomeID = 'us-gaap_NetIncomeLoss'
+        DilutedEPSID = 'us-gaap_EarningsPerShareDiluted'
 
+        tag = [NetSalesID, CostOfSalesID, NetMarginID, NetIncomeID, ResearchDevelopmentID, SGAID,\
+                OperatingExpensesID, OperatingIncomeID, IncomeBeforeProvisionIncomeTaxesID, ProvisionForIncomeTaxesID, NetIncomeID,DilutedEPSID]
+
+        OperatingDict = {'StockName': self.ticker, 'EPS':self.EPSList, NetSalesID:[], CostOfSalesID:[], NetMarginID: [], NetIncomeID:[], ResearchDevelopmentID:[],\
+                SGAID: [], OperatingExpensesID:[], OperatingIncomeID: [], IncomeBeforeProvisionIncomeTaxesID:[], ProvisionForIncomeTaxesID:[], NetIncomeID:[], DilutedEPSID:[], \
+                }
         for url in list(self.url_dict.values())[:9]:
             accession_number = url.split('&')[2][17:].replace('-', '')
+            
             try:
                 BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R2.htm"
                 request = {'url': BASE_URL, 'User-Agent': self.headers}
                 soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
-                        .prettify().split()[1].split("\n")[5].strip()
-                EPSList.append(soup.strip(' '))
+                        .prettify()
+
+
+
+                for i in tag:
+                   
+                    OperatingDict[i].append(soup.split(i)[1].split("\n")[5].strip())
             except:
                 BASE_URL = f"https://www.sec.gov/Archives/edgar/data/{self.get_CIK().strip('0')}/{accession_number}/R3.htm"
                 request = {'url': BASE_URL, 'User-Agent': self.headers}
                 soup = BeautifulSoup(requests.get(request['url'], headers={'User-Agent': request['User-Agent']}).content, 'lxml')\
-                        .prettify().split((NetSalesID, CostOfSalesID, NetMarginID, NetIncomeId, ResearchDevelopmentID, SGAID, OperatingExpensesID, OperatingIncome, OtherIncome, IncomeBeforeProvisionIncomeTaxesID, ProvisionFOrIncomeTaxesID, NetIncomeId, DilutedEPSID, TotalNetSalesID))[1].split("\n")[5].strip()
+                        .prettify()
 
-                EPSList.append(soup.strip(' '))
 
-        return [item for item in EPSList if item[0] == '$']
-9
+
+                for i in tag:
+                    OperatingDict[i].append(soup.split(i)[1].split("\n")[5].strip())
+
+        return OperatingDict 
+        
+
 
 
 
@@ -112,5 +112,5 @@ class Stockify:
         pass
 
 
-print(Stockify('amzn').get_EPS())
+print(Stockify('msft').get_10K_links())
 
